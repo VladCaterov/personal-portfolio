017497216e5e7ad356c6a7ba640ce00f65008fc1
Vladimir Caterov <vlad.caterov@mail.utoronto.ca>
2021-11-30 13:01:08 -0500

    initial startup of personal portfolio

---
+++ src/views/LandingPage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './landing-page.css'
+const LandingPage = () =>{
+ return (
+    <div className="landing-page-container">
+      <div className="background-image"></div>
+      <div className="content-container">
+         <div className="landing-page-header">
+         </div>
+
+         <div className="large-text-container">
+            <h1>Hello</h1>
+         </div>
+      </div>
+    </div>
+ );
+}
+
+export default LandingPage;
