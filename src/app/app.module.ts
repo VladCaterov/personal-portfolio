7d0fadc0874da92e107a1a901ec00c4b3043a52a
Vladimir Caterov <vladcaterovdev@gmail.com>
2023-07-24 20:37:29 -0500

    added material imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatButtonModule} from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './ui/homepage/homepage.component';
 import { ProjectListingComponent } from './ui/project-listing/project-listing.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SocialMediaLinkComponent } from './ui/social-media-link/social-media-link.component';
+import { MaterialImports } from './imported-materials';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
     ProjectListingComponent,
-    SocialMediaLinkComponent
+    SocialMediaLinkComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialImports
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/imported-materials.ts
@@ -0,0 +1,9 @@
+import { MatButtonModule } from "@angular/material/button";
+import { NgModule } from "@angular/core";
+
+@NgModule({
+    exports:[   
+        MatButtonModule
+    ]
+})
+export class MaterialImports{}
