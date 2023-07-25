edc60310f257a6a3c7a757431a0727c3eea5d5bb
Vladimir Caterov <vladcaterovdev@gmail.com>
2023-07-25 03:50:28 -0500

    working with tabs

--- src/app/imported-materials.ts
+++ src/app/imported-materials.ts
@@ -1,9 +1,13 @@
 import { MatButtonModule } from "@angular/material/button";
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from "@angular/material/icon"
 import { NgModule } from "@angular/core";
 
 @NgModule({
     exports:[   
-        MatButtonModule
+        MatButtonModule,
+        MatTabsModule,
+        MatIconModule
     ]
 })
 export class MaterialImports{}
